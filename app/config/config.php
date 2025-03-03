99c102bb4849b3a1e9179bf21b227b01fe5c5d4f
Valentin <150898364+Mavrokai@users.noreply.github.com>
2025-03-03 21:31:10 +0100

    Update config.php version NON-MAC

--- app/config/config.php
+++ app/config/config.php
@@ -13,15 +13,12 @@ $host = $_ENV['DB_HOST'];
 $dbname = $_ENV['DB_NAME'];
 $username = $_ENV['DB_USER'];
 $pswd = $_ENV['DB_PASS'];
-$port = $_ENV['PORT'];
 
-$socket = "/Applications/MAMP/tmp/mysql/mysql.sock";
 
 try {
-    // Initialisation correcte de $pdo
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;port=$port;unix_socket=$socket;charset=utf8", 
-                   $username, $pswd);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $pswd);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Échec de la connexion : " . $e->getMessage());
+    echo 'Erreur : ' . $e->getMessage();
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
