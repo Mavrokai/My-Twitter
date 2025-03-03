a252b4414eb4fe80c8dd65e93fcfbe357b0a9f7d
mavrokai <mavrokai.valentin.verscheure@epitech.eu>
2025-03-03 14:21:59 +0100

    [+] Partials sidebar

--- app/views/partials/sidebar.php
+++ app/views/partials/sidebar.php
@@ -1,8 +1,16 @@
-<div class="w-64 h-screen bg-[#59713E] border-r border-gray-200 p-4 fixed">
+<?php $items = ['Accueil', 'Explorer', 'Notifications', 'Messages', 'Suivi', 'Emplois', 'Communautés', 'Abonnement', 'Entreprise vé...', 'Profil', 'Plus']; ?>
+<?php $links = ['#', '#', '#', './messagerie.php', '#', '#', '#', '#', '#', '#']; ?>
+
+<div class="w-64 h-screen bg-[#f8f7f776] p-4 fixed rounded-2xl">
 
     <div class="space-y-2 mb-8">
-        <?php $items = ['Accueil', 'Explorer', 'Notifications', 'Messages', 'Suivi', 'Emplois', 'Communautés', 'Abonnement', 'Entreprise vé...', 'Profil', 'Plus']; ?>
-        <?php $links = ['#', '#', '#', './messagerie.php', '#', '#', '#', '#', '#', '#']; ?>
+
+        <div class="mt-[-0.9rem] w-full h-full">
+            <a href="">
+                <img class="w-18 h-18 mx-auto" src="../../../public/assets/img/LogoYjungle.webp" alt="Logo Yjungle">
+            </a>
+        </div>
+
         <?php foreach ($items as $key => $item): ?>
             <div class="flex items-center space-x-3 p-2 rounded-full hover:bg-blue-50 <?= $item === 'Messages' ? 'bg-blue-100 font-semibold' : '' ?> hover:cursor-pointer">
                 <a href="<?= $links[$key] ?>" class="text-gray-950">
