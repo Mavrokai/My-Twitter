3ed82b2baeb5ecc2c09ccb5aab15b8712bce976b
mavrokai <mavrokai.valentin.verscheure@epitech.eu>
2025-03-03 20:38:00 +0100

    [+] ajout favicon + img

--- app/views/notifs/notifications.php
+++ app/views/notifs/notifications.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notifications</title>
+    <link rel="stylesheet" href="../../../public/assets/img/LogoYjungle.webp">
     <link rel="stylesheet" href="../../../public/assets/css/all.css">
 </head>
 
